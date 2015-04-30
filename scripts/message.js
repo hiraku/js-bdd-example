06b0c0f9fead44d56f0c7c5604ee90247c6d2000
KURODA Hiraku <kuroda@syngram.co.jp>
2015-04-30 18:49:48 +0900

    Implement Message

--- scripts/message.js
+++ scripts/message.js
@@ -1,15 +1,22 @@
 define(["jquery"], function($){
 	function Message(confUrl){
+		this.confUrl = confUrl;
 	}
 
 	Message.prototype.get = function(){
 		var deferred = $.Deferred();
-		deferred.resolve();
+		var self = this;
+
+		$.get(this.confUrl).done(function(data){
+			self.text = data.message;
+			deferred.resolve(self.text);
+		});
 
 		return deferred.promise();
 	};
 
 	Message.prototype.put = function(element){
+		element.text(this.text);
 	};
 
 	return Message;
